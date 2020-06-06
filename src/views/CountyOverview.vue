a4daaf6b83186bfc1b4c5bc1091b403404af4c50
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-06-06 18:36:09 -0400

    Initial commit

---
+++ src/views/CountyOverview.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>CountyOverview</h1>
+    <iframe
+      src="https://covid19canada.maps.arcgis.com/apps/opsdashboard/index.html#/2772c0489f43411ca9493fc5888fbe67"
+      height="550"
+      width="100%"
+    ></iframe>
+    <p>Covid-19Tracker</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "CountyOverview"
+};
+</script>
+
+<style scoped>
+main {
+  padding: 100px;
+}
+</style>
